6e3cd16d2563b97262ba7e9535c74a46b6ab3c7f
Pedro Henrique Camargo <pedrohcamargon@gmail.com>
2025-07-23 20:33:39 -0300

    Paciente e Medico paginacao e ordernacao

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,11 +2,17 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -14,6 +20,12 @@ public class MedicoController {
 
     @Autowired
     private MedicoRepository medicoRepository;
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable) {
+        return medicoRepository.findAll(pageable)
+                .map(DadosListagemMedico::new);
+    }
 
     @PostMapping
     @Transactional
