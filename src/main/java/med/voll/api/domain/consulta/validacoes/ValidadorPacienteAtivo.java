017fbf72c5cf02a257f6d68cd5a4953972ee0b82
Pedro Henrique Camargo <pedrohcamargon@gmail.com>
2025-07-27 17:35:31 -0300

    feature/consulta: regras da clinica com classes da validacao

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorPacienteAtivo.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.paciente.PacienteRepository;
+
+public class ValidadorPacienteAtivo {
+
+    private PacienteRepository pacienteRepository;
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        if (dados.idPaciente() == null) {
+            return;
+        }
+
+        var pacienteAtivo = pacienteRepository.findAtivoById(dados.idPaciente());
+        if (!pacienteAtivo) {
+            throw new ValidacaoException("Consulta nao pode ser agendada com paciente excluido");
+        }
+    }
+}
